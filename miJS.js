313246e7ad2071f291498941e4d19a32fd088acc
ChampBloom <abinadijorge@gmail.com>
2020-06-24 15:26:02 -0500

    Version Final con Dark Mode

--- miJS.js
+++ miJS.js
@@ -1,1 +1,18 @@
+function toggleDarkMode() {
+    let darkButton = document.getElementById('body');
+    darkButton.classList.toggle('-dark');
+    console.log(darkButton);
+}
+
+function App() {
+    let darkMode = document.getElementById('button');
+
+    darkMode.addEventListener('click', toggleDarkMode);
+
+}
+
+
+document.addEventListener('DOMContentLoaded', App);
+
+
 console.log("JS Loaded");
